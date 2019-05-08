2e1025b6a370097d58a8bb170311141db8512a3a
TSITCL\s172050 <KengoKashihara1998@gmail.com>
2019-05-08 14:39:04 +0900

    スタートムービーの制作

--- SESA2019/Assets/SelectScene/Scripts/StarEffectController.cs
+++ SESA2019/Assets/SelectScene/Scripts/StarEffectController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class StarEffectController : MonoBehaviour
 {
-    ParticleSystem starEffect;
+    ParticleSystem starEffect; // 星のエフェクト
 
 	// Use this for initialization
 	void Start ()
@@ -18,15 +18,21 @@ public class StarEffectController : MonoBehaviour
         
 	}
 
+    /// <summary>
+    /// エフェクトの開始と停止
+    /// </summary>
+    /// <param name="isPlay"> true = "開始" / fasle = "停止" </param>
     public void Play(bool isPlay)
     {
         if (isPlay)
         {
+            // 開始
             if (starEffect.isPlaying) return;
             starEffect.Play();
         }
         else
         {
+            // 停止
             if (!starEffect.isPlaying) return;
             starEffect.Stop();
         }
---
+++ SESA2019/Assets/StartMove/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Transform effect;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        effect = transform.GetChild(0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        effect.GetComponent<StarEffectController>().Play(false);
+    }
+}
