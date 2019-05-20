9b387f30b198e6abd71b67d544ae012646ffcd9b
TSITCL\s172050 <KengoKashihara1998@gmail.com>
2019-05-20 13:15:57 +0900

    背景スクロールの実装(prefab)

---
+++ SESA2019/Assets/Test/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+	// Use this for initialization
+	void Start ()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rigid.velocity = new Vector2(5f, 0.0f);
+	}
+}
