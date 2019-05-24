cc1b891facca1468f21eff3855ef7cf274786adb
TSITCL\s172050 <KengoKashihara1998@gmail.com>
2019-05-24 17:01:40 +0900

    ゴールムービーの実装

---
+++ SESA2019/Assets/GoalScene/GoalController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour
+{
+    [SerializeField] Animator anime;
+    [SerializeField] ParticleSystem sparkle;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anime.StopPlayback();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void PlayEffect()
+    {
+        sparkle.Play();
+        StartCoroutine(IsChange());
+    }
+
+    private IEnumerator IsChange()
+    {
+        if (!sparkle.isPlaying) yield break;
+
+        while(sparkle.IsAlive(true))
+        {
+            yield return null;
+        }
+
+        SceneController.Instance.ChangeScene("SelectScene", 1.0f);
+    }
+}
