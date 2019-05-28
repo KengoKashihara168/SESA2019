ea7193136b13428e9f023ecb1a1e4657277f6fe3
KengoKashihara <kengokashihara1998@gmail.com>
2019-05-29 04:14:41 +0900

    ７

---
+++ SESA2019/Assets/GameScene/Script/SignBoard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignBoard : MonoBehaviour
+{
+    private bool _isOpened; // ウィンドウを開いている状態か
+
+    [SerializeField] private Transform  _playerTransform;
+    [SerializeField] private GameObject _messageWindow;
+
+
+    private void FixedUpdate()
+    {
+        if (_isOpened && Input.GetMouseButtonDown(0))
+        {
+            GameData.Instance().NoPose();
+            _messageWindow.SetActive(false);
+            _isOpened = false;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag.Equals("Circle"))
+        {
+            _isOpened = true;
+            GameData.Instance().Pose();
+            _messageWindow.SetActive(true);
+        }
+    }
+
+}
---
+++ SESA2019/Assets/Stage/氷/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    private bool moveFlag;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float length =  player.position.magnitude - transform.position.magnitude;
+        if (length <= 50.0f)
+        {
+            Vector2 move = new Vector2(-0.1f, 0.0f);
+            transform.Translate(move);
+        }
+
+        if(moveFlag)
+        {
+            
+        }
+	}
+
+    public void StartMove()
+    {
+        moveFlag = true;
+    }
+}
