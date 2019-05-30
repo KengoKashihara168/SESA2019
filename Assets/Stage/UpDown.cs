1863c0bd9014ff4c0809c7e6d8d57a6c383fc950
TSITCL\s172050 <KengoKashihara1998@gmail.com>
2019-05-30 12:32:14 +0900

    森ステージの実装

---
+++ Assets/Stage/UpDown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDown : MonoBehaviour
+{
+    [SerializeField] private bool  moveUpFlag;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveRange;
+    private Vector3 move;
+    private float moveDistance;
+
+	// Use this for initialization
+	void Start ()
+    {
+        move =  moveUpFlag ? Vector3.up : Vector3.down;
+        move *= moveSpeed;
+        moveDistance = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(moveDistance <= moveRange)
+        {
+            transform.position += move;
+            moveDistance += moveSpeed;
+        }
+        else
+        {
+            move *= -1.0f;
+            moveDistance = 0.0f;
+        }
+    }    
+}
