36eb2d420eb7c7f8b198d1829e4787450e1a02d7
TSITCL\s172050 <KengoKashihara1998@gmail.com>
2019-05-31 12:00:29 +0900

    スタート・ゴールのテキストに動きを追加＆暗闇ステージに敵を配置

--- Assets/Stage/Tutorial/Script/Meteo.cs
+++ Assets/Stage/Tutorial/Script/Meteo.cs
@@ -4,29 +4,12 @@ using UnityEngine;
 
 public class Meteo : StageObject
 {
-    private bool _flag; // 
-    [SerializeField] private Transform _meteoTransform; // 隕石のTransform
     [SerializeField] private float     _fallSpeed;      // 落下速度 
-
-    protected override void Init()
-    {
-        _flag = false;
-    }
 
     protected override void NoPoseFixedUpdate()
     {
-        if (!_flag) return;
-
-        var pos = _meteoTransform.position;
+        var pos = transform.position;
         pos.y -= _fallSpeed;
-        _meteoTransform.position = pos;
-    }
-
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.tag.Equals("Circle"))
-        {
-            _flag = true;
-        }
+        transform.position = pos;
     }
 }
